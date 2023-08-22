95ad9867b3d173d410e1e597dbbd1dd99d153f5f
Leonardo Silva <leonardoplxy@gmail.com>
2023-08-21 21:33:11 -0300

    create click to open menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('sidebar') sidebar: ElementRef;
   title = 'components';
+
+  toggleClasse() {
+    console.log('leo');
+    try {
+      const elemento = this.sidebar.nativeElement;
+      console.log(elemento);
+
+      // Verifica se a classe está presente no elemento
+      if (elemento.classList.contains('active')) {
+        // Se a classe estiver presente, remove-a
+        elemento.classList.remove('active');
+      } else {
+        // Se a classe não estiver presente, adiciona-a
+        elemento.classList.add('active');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
